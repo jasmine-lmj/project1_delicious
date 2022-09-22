363a14df4b3040a0b06d61b7d1210e15ed0d2645
Jasmine <jasminelo328@gmail.com>
2022-09-22 19:18:23 -0400

    added slideoutnave partial

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// testing branching here
+// magnifier search function 
+// const magnifier = document.querySelector('.fa-magnifying-glass');
+
+// magnifier.addEventListener('click', function(){
+// prompt('What are you searching for?');
+// })
+
+
+
+// When users click the hamburger menu
+
+// the menu slides out
+// the user selects where they want to go next 
+// then the browser navigate them to the destination  e.g. blog page
+// when click x , close the menu 
+
+
+
+
+const hamburger = document.querySelector('.hamburger');
+
+
+hamburger.addEventListener('click',function(e){
+    
+    console.log('great');
+} )
