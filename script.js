1d7eb7be7af2c084cf0df7242a9852fd6a7b1562
Jasmine <jasminelo328@gmail.com>
2022-09-22 20:54:51 -0400

    Finished slide nav for homepage

--- script.js
+++ script.js
@@ -6,9 +6,7 @@
 // })
 
 
-
 // When users click the hamburger menu
-
 // the menu slides out
 // the user selects where they want to go next 
 // then the browser navigate them to the destination  e.g. blog page
@@ -16,11 +14,19 @@
 
 
 
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburgerButton = document.querySelector('.hamburger');
 
-const hamburger = document.querySelector('.hamburger');
+    const menuCloseButton = document.querySelector('#menuClose');
+    const menu = document.querySelector('.slideOutNav');
 
+    hamburgerButton.addEventListener('click', (e) => {
+        menu.classList.add('open');
+    });
 
-hamburger.addEventListener('click',function(e){
-    
-    console.log('great');
-} )
+    menuCloseButton.addEventListener('click', (e) => {
+        menu.classList.remove('open');
+    })
+
+    console.log(hamburgerButton,menuCloseButton,menu)
+})
